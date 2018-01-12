bbb1f90fce785679eb3ae6db86b3df5c7a28b4be
Eke Wokocha <eke@mit.edu>
2018-01-11 22:31:11 -0500

    javascript decouple

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-
-/// Default Values for heatmap
-
-var googlefoodData = {max: 0, data: []};
-var googlecommunityData = {max: 0, data: []};
-var googlebigshopsData = {max: 0, data: []};
-var googlesmallshopsData = {max: 0, data: []};
-var googletransitData = {max: 0, data: []};
-var googletouristData = {max: 0, data: []};
-
-var yelpfoodData = {max: 0, data: []};
-var yelpshoppingData = {max: 0, data: []};
-var yelpcommunityData= {max: 0, data: []};
-
-var walkscoreData = {max: 0, data: []};
-var averageData = {max: 0, data: []};
-var compositeData = {max: 0, data: []};
-var markercontainer = new Array();
-
-var layersactive = [];
-
-
 //// Google Reverse Geocodeing Setup (for address display) ////
 var geocoder = new google.maps.Geocoder;
 
@@ -38,124 +16,12 @@ function geocodeLatLng(geocoder, lat, lng) {
     });
 }
 
-/////////////// Base Map Setup /////////////////////////////
-
-
-var baseLayer = L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-        maxZoom: 18
-    }
-);
-
-var map = new L.Map(document.getElementById('map'), {
-    center: new L.LatLng(42.3736, - 71.1097),
-    zoom: 13,
-    layers: [baseLayer]
-
-});
-
-map.on('click', onMapClick);
-
-
-
-var baseMaps = {
-    "Map View": baseLayer
-};
-
-
-/// Marker customization, chnage iconUrl if you want a different looking marker ///
-
-// var soofaIcon = L.icon({
-//     iconUrl: iconimage in png format,
-
-//     iconSize:     [38, 95], // size of the icon
-//     shadowSize:   [50, 64], // size of the shadow
-//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-//     shadowAnchor: [4, 62],  // the same for the shadow
-//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
-// });
-
-
-/// Add Markers to Map on click, plus change scores when marker is dragged///
-
-function onMapClick(e) {
-    marker = new L.marker(e.latlng, {draggable:'true'});
-    marker.on('click', markerOnClick);
-    markercontainer.push(marker);
-
-
-    for (var name in AllScores){
-        document.getElementById(name).value = calculatescore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]);
-    }
-
-    var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
-
-    marker.addTo(map)
-    map.panTo(marker.getLatLng());
-
-    marker.on('dragend', function(event){
-        var marker = event.target;
-        var position = marker.getLatLng();
-        marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
-        var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
-        marker.addTo(map);
-
-
-        for (var name in AllScores){
-            document.getElementById(name).value = calculatescore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]);
-        }
-
-        var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
-
-
-        map.panTo(new L.LatLng(position.lat, position.lng))
-
-    });
-
-    map.addLayer(marker);
-
-};
-
-/// Update scores on click ///
-function markerOnClick(e)
-{
-    for (var name in AllScores){
-        document.getElementById(name).value = calculatescore(e.latlng.lat,e.latlng.lng, AllScores[name]);
-    }
-
-    var currlocation = geocodeLatLng(geocoder, e.latlng.lat, e.latlng.lng);
-
-    map.panTo(new L.LatLng(e.latlng.lat, e.latlng.lng));
-}
-
-
-
-
 /// Heatmap Specifications ///
 
 
 /// If you want to have different colors, put this: gradient: {'.5': 'blue', '.8': 'red', '.95': 'white'}
 
-var cfg1 = {"radius": .007, "maxOpacity": .8, "scaleRadius": true, "useLocalExtrema": true, latField: 'lat', lngField: 'lng', valueField: 'count',  "blur": .8 };
 
-var compositelayer = new HeatmapOverlay(cfg1);
-
-
-/// Set Data ///
-
-
-compositelayer.setData({max: 0, data:[]});
-
-
-// /// Create Map Layers ///
-
-var compositegroup = L.layerGroup([compositelayer]);
-
-
-/// Add Layers to Map ///
-
-map.addLayer(compositegroup);
 
 
 //// Scoring Section ////
@@ -305,7 +171,7 @@ google.maps.event.addListener(searchBox, 'places_changed', function(){
 
         var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
 
-        marker.addTo(map)
+        marker.addTo(map);
         map.panTo(marker.getLatLng());
 
         marker.on('dragend', function(event){
@@ -313,7 +179,7 @@ google.maps.event.addListener(searchBox, 'places_changed', function(){
             var position = marker.getLatLng();
             marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
             var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
-            marker.addTo(map)
+            marker.addTo(map);
 
 
             for (var name in AllScores){
@@ -348,5 +214,3 @@ theButton.onclick = function() {
 
 // Code for Dashed Recntangle //
 
-var rect = L.rectangle([northeastcoord, southwestcoord], { dashArray: "10", color: "#4d4d4d",  opacity: .8,  fillOpacity: 0});
-map.addLayer(rect);
---
+++ js/map.js
@@ -0,0 +1,95 @@
+
+/////////////// Base Map Setup /////////////////////////////
+var baseLayer = L.tileLayer(
+    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }
+);
+
+var map = new L.Map(document.getElementById('map'), {
+    center: new L.LatLng(42.3736, - 71.1097),
+    zoom: 13,
+    layers: [baseLayer]
+
+});
+
+map.on('click', onMapClick);
+
+var baseMaps = {
+    "Map View": baseLayer
+};
+
+/// Add Markers to Map on click, plus change scores when marker is dragged///
+
+function onMapClick(e) {
+    marker = new L.marker(e.latlng, {draggable:'true'});
+    marker.on('click', markerOnClick);
+    markercontainer.push(marker);
+
+    for (var name in AllScores){
+        document.getElementById(name).value = calculatescore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]);
+    }
+
+    var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
+
+    marker.addTo(map)
+    map.panTo(marker.getLatLng());
+
+    marker.on('dragend', function(event){
+        var marker = event.target;
+        var position = marker.getLatLng();
+        marker.setLatLng(new L.LatLng(position.lat, position.lng),{draggable:'true'});
+        var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
+        marker.addTo(map);
+
+
+        for (var name in AllScores){
+            document.getElementById(name).value = calculatescore(marker.getLatLng().lat,marker.getLatLng().lng, AllScores[name]);
+        }
+
+        var currlocation = geocodeLatLng(geocoder, marker.getLatLng().lat, marker.getLatLng().lng);
+
+
+        map.panTo(new L.LatLng(position.lat, position.lng))
+
+    });
+
+    map.addLayer(marker);
+
+}
+
+/// Update scores on click ///
+function markerOnClick(e)
+{
+    for (var name in AllScores){
+        document.getElementById(name).value = calculatescore(e.latlng.lat,e.latlng.lng, AllScores[name]);
+    }
+
+    var currlocation = geocodeLatLng(geocoder, e.latlng.lat, e.latlng.lng);
+
+    map.panTo(new L.LatLng(e.latlng.lat, e.latlng.lng));
+}
+
+var cfg1 = {"radius": .007, "maxOpacity": .8, "scaleRadius": true, "useLocalExtrema": true, latField: 'lat', lngField: 'lng', valueField: 'count',  "blur": .8 };
+
+var compositelayer = new HeatmapOverlay(cfg1);
+
+
+/// Set Data ///
+
+
+compositelayer.setData({max: 0, data:[]});
+
+
+// /// Create Map Layers ///
+
+var compositegroup = L.layerGroup([compositelayer]);
+
+
+/// Add Layers to Map ///
+
+map.addLayer(compositegroup);
+
+var rect = L.rectangle([northeastcoord, southwestcoord], { dashArray: "10", color: "#4d4d4d",  opacity: .8,  fillOpacity: 0});
+map.addLayer(rect);
